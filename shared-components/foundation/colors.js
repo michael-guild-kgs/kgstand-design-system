7ff11749755e7a698326939a768a0d9505fd3aae
Michael Guild <michag11@Michaels-MacBook-Pro.local>
2021-03-12 11:21:29 -0700

    colors and index

--- shared-components/foundation/colors.js
+++ shared-components/foundation/colors.js
@@ -1,3 +1,15 @@
+/* ------ brand colors ------ */
+export const BrandPrimary = 'rgba(44, 88, 117, 1)'
+
+export const Brand1 = 'rgba(170, 188, 200, 1)'
+export const Brand2 = 'rgba(128, 154, 172, 1)' 
+export const Brand3 = 'rgba(86, 121, 144, 1)'
+export const Brand4 = 'rgba(35, 69, 92, 1)'
+export const Brand5 = 'rgba(25, 51, 68, 1)'
+export const Brand6 = 'rgba(16, 32, 43, 1)'
+
+/* ------ grayscale colors ------ */
+/* 25% variations of black */
 export const Black = 'rgba(0, 0, 0, 1)'
 export const Gray1 = 'rgba(51, 51, 51, 1)'
 export const Gray2 = 'rgba(102, 102, 102, 1)'
@@ -5,3 +17,12 @@ export const Gray3 = 'rgba(153, 153, 153, 1)'
 export const Gray4 = 'rgba(204, 204, 204, 1)'
 export const Gray5 = 'rgba(242, 242, 242, 1)'
 export const White = 'rgba(255, 255, 255, 1)'
+
+/* ------ alert colors ------ */
+const Green = 'rgba(77, 204, 134, 1)'
+const Yellow = 'rgba(239, 190, 57, 1)'
+const Red = 'rgba(223, 53, 57, 1)'
+
+export const Success = Green
+export const Alert = Yellow 
+export const Warning = Red
